e3d554e8b418221730140e1c659931138c624082
Harsh <harsh.pandey@degreefyd.com>
2026-04-07 12:09:55 +0530

    new key

--- controllers/leadactivity.controller.js
+++ controllers/leadactivity.controller.js
@@ -52,7 +52,7 @@ export const createLeadActivity = async (leadData, studentId) => {
       is_transfer: leadData.isTransfer || false,
       source: source,
       source_url: sourceurl,
-      lead_type: leadData.leadType || "",
+      lead_type: leadData.lead_type || "",
       preferred_college_cll: leadData.preferred_college_cll || [],
       utm_source: leadData.utmSource || "",
       utm_medium: leadData.utmMedium || "",

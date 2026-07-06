12e0eabf7b222814a67160642471e89785e266e8
ChandrasekharBhanuri <bhanuri.chandrashekhar@degreefyd.com>
2026-07-06 15:38:08 +0530

    pause:paused the lpu cron job for next 2-3 days as per instructions

--- cronScheduler.js
+++ cronScheduler.js
@@ -11,7 +11,7 @@ import databaseConnection from './config/database-connection.js';
 import startSyncMissingLeadsCron from './cron/syncMissingLeadsCron.js';
 import startMidnightCleanupCron from './cron/midnightCleanupCron.js';
 // import startLeadSwapCron from './cron/leadSwapCron.js';
-import startLpuPhagwaraRegularCron from './cron/lpuPhagwaraRegularCron.js';
+// import startLpuPhagwaraRegularCron from './cron/lpuPhagwaraRegularCron.js';
 
 async function startScheduler() {
   console.log('⏰ Regular LMS Cron Scheduler initializing...');
@@ -21,7 +21,7 @@ async function startScheduler() {
   startSyncMissingLeadsCron();
   startMidnightCleanupCron();
   // startLeadSwapCron();
-  startLpuPhagwaraRegularCron();
+  // startLpuPhagwaraRegularCron();
 
   console.log('✅ Regular LMS Cron Scheduler running.');
 }
